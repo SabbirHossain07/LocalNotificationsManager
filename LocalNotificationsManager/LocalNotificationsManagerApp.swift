9b7074cedf4a419ecca09a8f0ba082f85303e5e7
Sabbir Hossain <sabbirsopnil07@gmail.com>
2025-12-02 11:25:16 +0600

    Initial Commit

---
+++ LocalNotificationsManager/LocalNotificationsManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNotificationsManagerApp.swift
+//  LocalNotificationsManager
+//
+//  Created by Sopnil Sohan on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LocalNotificationsManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
